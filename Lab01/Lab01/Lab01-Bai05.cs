24d779667dbf0872379f1d9c70858af96cfb41e4
j3un3t0nas <22521311@gm.uit.edu.vn>
2024-04-11 02:35:40 +0700

    lab01

---
+++ Lab01/Lab01/Lab01-Bai05.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class Lab01_Bai05 : Form
+    {
+        public Lab01_Bai05()
+        {
+            InitializeComponent();
+        }
+
+        int factorial( int n )
+        {
+            if (n == 1|| n == 0)
+                return 1;
+            
+            return n*(n-1);
+        }
+
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumA.Text) == true || string.IsNullOrEmpty(txtNumB.Text) == true)
+            {
+                MessageBox.Show("Vui lòng nhập các giá trị hợp lệ");
+                return;
+            }
+
+            int num1 = 0, num2 = 0;
+
+            if( int.TryParse(txtNumA.Text.Trim(), out num1) && int.TryParse(txtNumB.Text.Trim(), out num2))
+            {
+                num1 = Int32.Parse(txtNumA.Text.Trim());
+                num2 = Int32.Parse(txtNumB.Text.Trim());
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên !");
+                return;
+            }
+
+            if(cboChucNang.SelectedIndex == -1)
+            {
+                MessageBox.Show("Vui lòng chọn chức năng thực hiện!");
+                return;
+            }  
+            else
+            {
+                if(cboChucNang.SelectedIndex == 0)
+                {
+                    string str1 = "";
+                    int diff = num2 - num1;
+                    str1 = "Bảng cửu chương B - A : \r\n";
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        str1 += (num2 - num1) + " x " + i + " = " + (diff * i) + "\r\n";
+                    }
+
+                    txtKetQua.Text = str1;
+                } 
+                if(cboChucNang.SelectedIndex == 1)
+                {
+                    if (num1 < num2)
+                    {
+                        MessageBox.Show("Không thể tính giai thừa do A < B !");
+                        return;
+                    }
+
+                    double ans1 = factorial(num1 - num2);
+                    double ans2 = 0;
+                    String str1 = " ";
+                    for(int i = 1; i <= num2; i++)
+                    {
+                        ans2 += Math.Pow(num1, i);
+                    }
+
+                    str1 = "(A - B)! = " + ans1 + "\r\n";
+                    str1 += "Tổng S = " + ans2 + "\r\n";
+                    txtKetQua.Text = str1;
+
+                }    
+            }    
+          
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            foreach( Control ctrl in this.Controls )
+            {
+                if( ctrl is TextBox)
+                {
+                    ((TextBox)ctrl).Clear();
+                }
+            }
+            txtKetQua.Clear();
+        }
+    }
+}
---
+++ Lab01/Lab01/Lab01-Bai06.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class Lab01_Bai06 : Form
+    {
+        public Lab01_Bai06()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            int dd, mm, yy;
+            if(int.TryParse(txtDate.Text.Trim(), out dd) && int.TryParse(txtMonth.Text.Trim(), out mm) && int.TryParse(txtYear.Text.Trim(), out yy))
+            {
+                DateTime curentTime = DateTime.Now;
+                string cungHoangDao = "";
+                dd = Int32.Parse(txtDate.Text.Trim());
+                mm = Int32.Parse(txtMonth.Text.Trim());
+
+                switch (mm)
+                {
+                    case 1:
+                        if( dd >= 1 && dd <=20 )
+                        {
+                            cungHoangDao = "Cung Ma Kết";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Bảo Bình";
+                        }
+                        break;
+                    case 2:
+                        if( dd >= 1 && dd <= 19)
+                        {
+                            cungHoangDao = "Cung Bảo Bình";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Song Ngư";
+                        }
+                        break;
+                    case 3:
+                        if (dd >= 1 && dd <= 20)
+                        {
+                            cungHoangDao = "Cung Song Ngư";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Bạch Dương";
+                        }
+                        break;
+                    case 4:
+                        if (dd >= 1 && dd <= 20)
+                        {
+                            cungHoangDao = "Cung Bạch Dương";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Kim Ngưu";
+                        }
+                        break;
+                    case 5:
+                        if (dd >= 1 && dd <= 21)
+                        {
+                            cungHoangDao = "Cung Kim Ngưu";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Song Tử";
+                        }
+                        break;
+                    case 6:
+                        if (dd >= 1 && dd <= 21)
+                        {
+                            cungHoangDao = "Cung Song Tử";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Cự Giải";
+                        }
+                        break;
+                    case 7:
+                        if (dd >= 1 && dd <= 22)
+                        {
+                            cungHoangDao = "Cung Cự Giải";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Sư Tử";
+                        }
+                        break;
+                    case 8:
+                        if (dd >= 1 && dd <= 22)
+                        {
+                            cungHoangDao = "Cung Sư Tử";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Xử Nữ";
+                        }
+                        break;
+                    case 9:
+                        if (dd >= 1 && dd <= 23)
+                        {
+                            cungHoangDao = "Cung Xử Nữ";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Thiên Bình";
+                        }
+                        break;
+                    case 10:
+                        if (dd >= 1 && dd <= 23)
+                        {
+                            cungHoangDao = "Cung Thiên Bình";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Thần Nông";
+                        }
+                        break;
+                    case 11:
+                        if (dd >= 1 && dd <= 22)
+                        {
+                            cungHoangDao = "Cung Thần Nông";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Nhân Mã";
+                        }
+                        break;
+                    case 12:
+                        if (dd >= 1 && dd <= 21)
+                        {
+                            cungHoangDao = "Cung Nhân Mã";
+                        }
+                        else
+                        {
+                            cungHoangDao = "Cung Ma Kết";
+                        }
+                        break;
+                    default:
+                            MessageBox.Show("Ngày hoặc tháng không hợp lê. Vui lòng nhập lại!");
+                            return;
+                }
+
+                txtKetQua.Text = "Cung hoàng đạo của bạn là : " + cungHoangDao + "\r\n";
+                txtKetQua.Text += "Tuổi hiện tại của bạn là : " + (curentTime.Year - yy);
+            }
+            else
+            {
+                MessageBox.Show("Hãy nhập đúng định dạng ngày , tháng, năm !");
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            foreach( Control ctrl in this.Controls )
+            {
+                if( ctrl is TextBox )
+                {
+                    ((TextBox)ctrl).Clear();
+                }
+                txtKetQua.Clear();
+            }    
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
